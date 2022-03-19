86a4352826976cafb3c210694986a76603aaf3df
Curly Brackets <water.curly@outlook.com>
2022-03-19 20:08:08 +0800

    borrow limit used & health

--- src/main.ts
+++ src/main.ts
@@ -3,28 +3,19 @@ import cEthAbi from "./abis/cEth.json";
 import cTokenAbi from "./abis/cToken.json";
 import compAbi from "./abis/comptroller.json";
 import erc20Abi from "./abis/erc20.json";
+import priceFeedAbi from "./abis/priceFeed.json";
 import { Erc20 } from "./abis/types";
 const provider = new providers.JsonRpcProvider(
   "https://kovan.infura.io/v3/2b17a18942384c25ad91e8636764a89f"
 );
+
+// Just Test Account
 const privateKey =
   "4f4b5c6ddc896de134f2a8e3d0cc5925a15e2b20656eab74f423b31eaf14d69c";
 const wallet = new Wallet(privateKey, provider);
 const myWalletAddress = wallet.address;
-
-// Mainnet Contract for cDAI (https://compound.finance/docs#networks)
-const assetName = "DAI"
-const cTokenContractAddress = "0xF0d0EB522cfa50B716B3b1604C4F0fA6f04376AD";
-const cTokenContract = new Contract(cTokenContractAddress, cTokenAbi, wallet);
-const underlyingContractAddress = "0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa";
-const underlyingContract = new Contract(
-  underlyingContractAddress,
-  erc20Abi,
-  wallet
-) as Erc20;
-const underlyingDecimals = 18; // Number of decimals defined in this ERC20 token's contract
-const cEthContractAddress = "0x41b5844f4680a8c38fbb695b7f9cfd1f64474a72";
-const cEthContract = new Contract(cEthContractAddress, cEthAbi, wallet);
+const priceFeedAddress = "0xbBdE93962Ca9fe39537eeA7380550ca6845F8db7";
+const priceFeed = new Contract(priceFeedAddress, priceFeedAbi, wallet);
 const comptrollerContractAddress = "0x5eae89dc1c671724a672ff0630122ee834098657";
 const comptrollerContract = new Contract(
   comptrollerContractAddress,
@@ -32,24 +23,130 @@ const comptrollerContract = new Contract(
   wallet
 );
 
-const main = async function () {
+const allMarkets = [
+  {
+    symbol: "DAI",
+    underlyingAddress: "0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa",
+    address: "0xF0d0EB522cfa50B716B3b1604C4F0fA6f04376AD",
+    decimals: 18,
+  },
+  {
+    symbol: "ETH",
+    underlyingAddress: "0x0000000000000000000000000000000000000000",
+    address: "0x41B5844f4680a8C38fBb695b7F9CFd1F64474a72",
+    decimals: 18,
+  },
+  {
+    symbol: "BAT",
+    underlyingAddress: "0x482dC9bB08111CB875109B075A40881E48aE02Cd",
+    address: "0x4a77fAeE9650b09849Ff459eA1476eaB01606C7a",
+    decimals: 18,
+  },
+];
+
+const summary = async function () {
   // Supply APY
-  const supplyRatePerBlock = await cEthContract.callStatic.supplyRatePerBlock();
+  const cDAIAddress = allMarkets[0].address;
+  const cDAIContract = new Contract(cDAIAddress, cTokenAbi, wallet);
+  const supplyRatePerBlock = await cDAIContract.callStatic.supplyRatePerBlock();
   const supplAPY = calculateAPY(supplyRatePerBlock);
-  console.log(supplAPY);
+  console.log("supply APY:", supplAPY);
 
   // Distribution APY
-  const compSpeed = await comptrollerContract.compSpeeds(cTokenContractAddress);
-  console.log(compSpeed);
+  const compSpeed = await comptrollerContract.compSpeeds(cDAIAddress);
+  console.log("Distribution APY:", compSpeed);
 
   // Collateral Factor
   let { 1: collateralFactor } = await comptrollerContract.callStatic.markets(
-    cTokenContractAddress
+    cDAIAddress
   );
   collateralFactor = (collateralFactor / 1e18) * 100; // Convert to percent
-  console.log(collateralFactor);
+  console.log("Collateral Factor:", collateralFactor);
 
+  // 供给余额乘以市场的抵押物因子，然后求和;然后减去借款余额，得到相等的账户流动性
+
+  // Current Liquidity
+  const liquidity = await comptrollerContract.callStatic.getAccountLiquidity(
+    wallet.getAddress()
+  );
+  const currentLiquidity = +liquidity[1] / Math.pow(10, 18);
+  console.log("Liquidity:", currentLiquidity);
+
+  const totalBorrowed = (
+    await Promise.all(
+      allMarkets.map(async ({ address, symbol, decimals }: any) => {
+        const cToken = new Contract(address, cTokenAbi, wallet);
+        let underlyingPriceInUsd = await priceFeed.callStatic.price(symbol);
+        underlyingPriceInUsd = underlyingPriceInUsd / 1e6; // Price feed provides price in USD with 6 decimal places
+        const borrowBalance = await cToken.callStatic.borrowBalanceCurrent(
+          myWalletAddress
+        );
+        return underlyingPriceInUsd * (+borrowBalance / Math.pow(10, decimals));
+      })
+    )
+  ).reduce((sum, usd) => sum + usd, 0);
+
+  console.log("Borrowed:", totalBorrowed);
+  console.log(
+    "Borrowed Limit:",
+    totalBorrowed + currentLiquidity,
+  );
+  // Borrow Limit Used
+  console.log(
+    "Borrow Limit Used:",
+    (totalBorrowed / (totalBorrowed + currentLiquidity)) * 100
+  );
+  // Health
+  console.log("Health:", (totalBorrowed + currentLiquidity) / totalBorrowed);
+};
+
+const collateral = async function () {
+  // Use xxx as Collateral
+  // to Collateral cDAI
+  const cTokenContractAddress = allMarkets[0].address;
+  const assetName = allMarkets[0].symbol;
+
+  const collaterals = await comptrollerContract.getAssetsIn(
+    wallet.getAddress()
+  );
+  console.log(collaterals);
+  if (!collaterals.includes(cTokenContractAddress)) {
+    const enterMarkets = await comptrollerContract.enterMarkets([
+      cTokenContractAddress,
+    ]);
+    await enterMarkets.wait(1);
+    console.log(`Use c${assetName} as Collateral`);
+  } else {
+    console.log(`c${assetName} already used as Collateral`);
+  }
+};
+
+const revokeCollateral = async function () {
+  // Revoke xxx as Collateral
+  const cTokenContractAddress = allMarkets[0].address;
+  const assetName = allMarkets[0].symbol;
+
+  const exitMarkets = await comptrollerContract.exitMarket(
+    cTokenContractAddress
+  );
+  await exitMarkets.wait(1);
+  console.log(`Revoke c${assetName} as Collateral`);
+  console.log(await comptrollerContract.getAssetsIn(wallet.getAddress()));
+};
+
+const supply = async function () {
   // isApproved
+  const cTokenContractAddress = allMarkets[0].address;
+  const assetName = allMarkets[0].symbol;
+  const underlyingContractAddress = allMarkets[0].underlyingAddress;
+  const underlyingDecimals = allMarkets[0].decimals;
+  const underlyingContract = new Contract(
+    underlyingContractAddress,
+    erc20Abi,
+    wallet
+  );
+  const cTokenContract = new Contract(cTokenContractAddress, cTokenAbi, wallet);
+
   const allowance = await underlyingContract.callStatic.allowance(
     await wallet.getAddress(),
     cTokenContractAddress
@@ -72,59 +169,104 @@ const main = async function () {
     console.log("is Enable");
   }
 
-  // Use xxx as Collateral
-  // to Collateral cDAI
-  const collaterals = await comptrollerContract.getAssetsIn(
-    wallet.getAddress()
-  );
-  console.log(collaterals);
-  if (!collaterals.includes(cTokenContractAddress)) {
-    const enterMarkets = await comptrollerContract.enterMarkets([
-      cTokenContractAddress,
-    ]);
-    await enterMarkets.wait(1);
-    console.log(`Use c${assetName} as Collateral`);
-  } else {
-    console.log(`c${assetName} already used as Collateral`);
-  }
-
-  // Revoke xxx as Collateral
-  const exitMarkets = await comptrollerContract.exitMarket(
-    cTokenContractAddress
-  );
-  await exitMarkets.wait(1);
-  console.log(`Revoke c${assetName} as Collateral`);
-  console.log(await comptrollerContract.getAssetsIn(wallet.getAddress()));
-
   // Supply
   // Mint cTokens by supplying underlying tokens to the Compound Protocol
   const tx = await cTokenContract.mint(underlyingToSupply.toString());
   await tx.wait(1); // wait until the transaction has 1 confirmation on the blockchain
-  console.log(`c${assetName} "Mint" operation successful.`, '\n');
+  console.log(`c${assetName} "Mint" operation successful.`, "\n");
 
   // Supply underlyingToken Balance
-  const bal = await cTokenContract.callStatic.balanceOfUnderlying(myWalletAddress);
+  const bal = await cTokenContract.callStatic.balanceOfUnderlying(
+    myWalletAddress
+  );
   const balanceOfUnderlying = +bal / Math.pow(10, underlyingDecimals);
-  console.log(`${assetName} supplied to the Compound Protocol:`, balanceOfUnderlying, '\n');
+  console.log(
+    `${assetName} supplied to the Compound Protocol:`,
+    balanceOfUnderlying,
+    "\n"
+  );
 
   // Wallet cToken Balance
-  let cTokenBalance = +(await cTokenContract.callStatic.balanceOf(myWalletAddress)) / 1e8;
+  let cTokenBalance =
+    +(await cTokenContract.callStatic.balanceOf(myWalletAddress)) / 1e8;
   console.log(`My wallet's c${assetName} Token Balance:`, cTokenBalance);
 
   // Wallet underlyingToken Balance
-  let underlyingBalance = await underlyingContract.callStatic.balanceOf(myWalletAddress);
-  let underlyingBalanceNumber = +underlyingBalance / Math.pow(10, underlyingDecimals);
-  console.log(`My wallet's ${assetName} Token Balance:`, underlyingBalanceNumber, '\n');
+  const underlyingBalance = await underlyingContract.callStatic.balanceOf(
+    myWalletAddress
+  );
+  const underlyingBalanceNumber =
+    +underlyingBalance / Math.pow(10, underlyingDecimals);
+  console.log(
+    `My wallet's ${assetName} Token Balance:`,
+    underlyingBalanceNumber,
+    "\n"
+  );
+};
+
+const withdraw = async () => {
+  const cTokenContractAddress = allMarkets[0].address;
+  const assetName = allMarkets[0].symbol;
+  const underlyingContractAddress = allMarkets[0].underlyingAddress;
+  const underlyingDecimals = allMarkets[0].decimals;
+  const underlyingContract = new Contract(
+    underlyingContractAddress,
+    erc20Abi,
+    wallet
+  );
+  const cTokenContract = new Contract(cTokenContractAddress, cTokenAbi, wallet);
+
+  const bal = await cTokenContract.callStatic.balanceOfUnderlying(
+    myWalletAddress
+  );
+  const balanceOfUnderlying = +bal / Math.pow(10, underlyingDecimals);
+  console.log(
+    `${assetName} supplied to the Compound Protocol:`,
+    balanceOfUnderlying,
+    "\n"
+  );
 
   // Withdraw
+  const inputToWithdraw = "0.05";
+  if (+inputToWithdraw > balanceOfUnderlying) {
+    console.log("Insufficient Liquidity");
+  }
   console.log(`Redeeming the c${assetName} for ${assetName}...`);
-  let underlyingAmount = balanceOfUnderlying * Math.pow(10, underlyingDecimals);
-  const withdrawTx = await cTokenContract.redeemUnderlying(underlyingAmount.toString());
+  let underlyingAmount = +inputToWithdraw * Math.pow(10, underlyingDecimals);
+  const withdrawTx = await cTokenContract.redeemUnderlying(
+    underlyingAmount.toString()
+  );
   await withdrawTx.wait(1); // wait until the transaction has 1 confirmation on the blockchain
-  underlyingBalance = await underlyingContract.callStatic.balanceOf(myWalletAddress);
-  underlyingBalanceNumber = +underlyingBalance / Math.pow(10, underlyingDecimals);
-  console.log(`My wallet's ${assetName} Token Balance:`, underlyingBalanceNumber, '\n');
+  const underlyingBalance = await underlyingContract.callStatic.balanceOf(
+    myWalletAddress
+  );
+  const underlyingBalanceNumber =
+    +underlyingBalance / Math.pow(10, underlyingDecimals);
+  console.log(
+    `My wallet's ${assetName} Token Balance:`,
+    underlyingBalanceNumber,
+    "\n"
+  );
+};
 
+const borrow = async () => {
+  const cTokenContractAddress = allMarkets[0].address;
+  const assetName = allMarkets[0].symbol;
+  const underlyingDecimals = allMarkets[0].decimals;
+  const cTokenContract = new Contract(cTokenContractAddress, cTokenAbi, wallet);
+
+  const underlyingToBorrow = 0.1;
+  console.log(`Now attempting to borrow ${underlyingToBorrow} ${assetName}...`);
+  const scaledUpBorrowAmount = (
+    underlyingToBorrow * Math.pow(10, underlyingDecimals)
+  ).toString();
+  const trx = await cTokenContract.borrow(scaledUpBorrowAmount);
+  await trx.wait(1);
+  // console.log('Borrow Transaction', trx);
+};
+
+const main = async () => {
+  await summary();
 };
 
 main().catch((err) => {
